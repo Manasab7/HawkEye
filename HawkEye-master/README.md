2225c596129a33cb01124dfd15a08a6054fd5340
Manasab7 <b.manasa777@gmail.com>
2018-01-13 11:31:53 -0500

    First Commit!

---
+++ HawkEye-master/README.md
@@ -0,0 +1,2 @@
+# HawksEye
+This is an eye tracking application which detects the eyes of the user via Webcam.It is developed in Python using OpenCV and XML.
---
+++ HawkEye-master/video_traking_eydetection.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+
+
+# @author(s)
+# Jaspreet Singh (singh1i6@uwindsor.ca)
+# Venu Srinivasa Reddy (srinivav@uwindsor.ca)
+# Manasa Gowthami Bogavalli (bogaval@uwindsor.ca)
+# Rakesh Chandra Pandaraboina (pandara@uwidsor.ca)
+# Ganeshan Kiran Kumar (ganeshak@uwindsor.ca)
+# @date Monday, 20 November 2017
+
+
+
+import numpy as np
+import cv2
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(255,0,255),2)
+
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
